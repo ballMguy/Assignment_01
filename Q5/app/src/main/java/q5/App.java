cf276f66f0b5aefdd76d4319abc0601c716ec7e7
ballMguy <supaluk29188@gmail.com>
2024-12-04 11:30:19 +0700

    work

--- Q5/app/src/main/java/q5/App.java
+++ Q5/app/src/main/java/q5/App.java
@@ -9,9 +9,7 @@ public class App {
     }
 
     public static void main(String[] args) {
-        Scanner scanIn = new Scanner(System.in);
         int x = 5;
-        scanIn.close();
         for (int i = 1; i <= x; i++) {
 
             for (int j = 1; j <= i; j++) {
